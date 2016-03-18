817bc8c109ed18f168619a780361d7835aef6fd4
Mike Pham <mpham@nativecode.com>
2016-03-18 03:53:15 -0500

    Fixed up AssemblyInfo types for nuget packaging.

--- src/NativeCode.Core.Data/Properties/AssemblyInfo.cs
+++ src/NativeCode.Core.Data/Properties/AssemblyInfo.cs
@@ -1,30 +1,17 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NativeCode.Core.Data")]
-[assembly: AssemblyDescription("")]
+using NativeCode.Core;
+
+[assembly: AssemblyCompany(CoreConstants.Company)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NativeCode.Core.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright(CoreConstants.Copyright)]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion(CoreConstants.AssemblyVersion)]
+[assembly: AssemblyProduct("NativeCode.Core.Data")]
+[assembly: AssemblyTitle("NativeCode.Core.Data")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ src/NativeCode.Core/CoreConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace NativeCode.Core
+{
+    public static class CoreConstants
+    {
+#if RELEASE
+        public const string AssemblyVersion = "1.0.0-release";
+#else
+        public const string AssemblyVersion = "1.0.0-dev";
+#endif
+
+        public const string Company = "NativeCode Development";
+
+        public const string Copyright = "Copyright © NativeCode Development 2016";
+    }
+}
