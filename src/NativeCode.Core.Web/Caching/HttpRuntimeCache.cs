6de90dc6bb3cd832d0c44f9b163f9e5e34f99121
Mike Pham <mpham@nativecode.com>
2016-07-30 04:24:54 -0400

    adding HttpRuntimeCache

---
+++ src/NativeCode.Core.Web/Caching/HttpRuntimeCache.cs
@@ -0,0 +1,32 @@
+﻿namespace NativeCode.Core.Web.Caching
+{
+    using System;
+    using System.Web;
+
+    using NativeCode.Core.Caching;
+
+    public class HttpRuntimeCache<T> : ICache<T>
+    {
+        public T Get(string key, T defaultValue = default(T))
+        {
+            return (T)HttpRuntime.Cache[key];
+        }
+
+        public bool Get(string key, out T value)
+        {
+            return (value = (T)HttpRuntime.Cache[key]).Equals(default(T)) == false;
+        }
+
+        public void Set(string key, T value, TimeSpan? duration = null)
+        {
+            if (duration.HasValue)
+            {
+                HttpRuntime.Cache.Insert(key, value, null, DateTime.UtcNow.Add(duration.Value), TimeSpan.Zero);
+            }
+            else
+            {
+                HttpRuntime.Cache.Insert(key, value);
+            }
+        }
+    }
+}
--- src/NativeCode.Core/Caching/ICache.cs
+++ src/NativeCode.Core/Caching/ICache.cs
@@ -2,12 +2,34 @@
 {
     using System;
 
+    /// <summary>
+    /// Provides a contract to provide caching facilities.
+    /// </summary>
+    /// <typeparam name="T">The type to cache.</typeparam>
     public interface ICache<T>
     {
+        /// <summary>
+        /// Gets the value of the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>Returns the cached value.</returns>
         T Get(string key, T defaultValue = default(T));
 
+        /// <summary>
+        /// Gets the value of the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns><c>true</c> if value retrieved, <c>false</c> otherwise.</returns>
         bool Get(string key, out T value);
 
+        /// <summary>
+        /// Sets the value for the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="duration">The duration.</param>
         void Set(string key, T value, TimeSpan? duration = default(TimeSpan?));
     }
 }
