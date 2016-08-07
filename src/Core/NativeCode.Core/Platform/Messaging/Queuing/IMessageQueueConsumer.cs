6ef9cd414cb79b80ee6494d3057cfdd708cd4d65
Mike Pham <mpham@nativecode.com>
2016-08-07 03:13:31 -0400

    adding disposable interface to consumer

--- src/Core/NativeCode.Core/Platform/Messaging/Queuing/IMessageQueueConsumer.cs
+++ src/Core/NativeCode.Core/Platform/Messaging/Queuing/IMessageQueueConsumer.cs
@@ -4,7 +4,7 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IMessageQueueConsumer
+    public interface IMessageQueueConsumer : IDisposable
     {
         Task StartAsync(Uri url, CancellationToken cancellationToken);
     }
