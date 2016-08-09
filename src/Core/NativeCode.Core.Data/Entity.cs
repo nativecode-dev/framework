21433c7545d1bd397aa9c40386efdad73895bbf1
Mike Pham <mpham@nativecode.com>
2016-08-09 15:49:50 -0400

    changed Entity<T> to implement from IEntity<T>

--- src/Core/NativeCode.Core.Data/Entity.cs
+++ src/Core/NativeCode.Core.Data/Entity.cs
@@ -15,37 +15,32 @@
 
         public string UserModified { get; protected set; }
 
-        public void SetDateCreated(DateTimeOffset value)
+        void IEntityAuditor.SetDateCreated(DateTimeOffset value)
         {
             this.DateCreated = value;
         }
 
-        public void SetDateModified(DateTimeOffset value)
+        void IEntityAuditor.SetDateModified(DateTimeOffset value)
         {
             this.DateModified = value;
         }
 
-        public void SetUserCreated(IIdentity identity)
+        void IEntityAuditor.SetUserCreated(IIdentity identity)
         {
             this.UserCreated = identity.Name;
         }
 
-        public void SetUserModified(IIdentity identity)
+        void IEntityAuditor.SetUserModified(IIdentity identity)
         {
             this.UserModified = identity.Name;
         }
     }
 
     [SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Generic type.")]
-    public abstract class Entity<T> : Entity
-        where T : struct
+    public abstract class Entity<TKey> : Entity, IEntity<TKey>
+        where TKey : struct
     {
         [Key]
-        public T Id { get; set; }
-
-        public void SetId(T id)
-        {
-            this.Id = id;
-        }
+        public TKey Id { get; set; }
     }
 }
